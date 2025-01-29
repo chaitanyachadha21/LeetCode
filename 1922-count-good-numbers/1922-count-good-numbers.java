5f1a995778498ca5397fa099f4030b325754ba7f
Chaitanya Chadha <122335933+chaitanyachadha21@users.noreply.github.com>
2025-01-29 23:34:16 +0530

    Time: 0 ms (100%), Space: 40.7 MB (28.33%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    int mod=(int)1e9+7;
+    public int countGoodNumbers(long n) {
+        long even=(n+1)/2;
+        long odd=n/2;
+        long a=pow(4,odd)%mod;
+        long b=pow(5,even)%mod;
+        return (int)((a*b)%mod);
+    }
+
+    public long pow(long a,long b){
+        if(b==0){
+            return 1;
+        }
+        long temp=pow(a,b/2);
+        if(b%2==0){
+            return (temp*temp)%mod;
+        }
+        else{
+            return (a *temp*temp)%mod;
+        }
+    }
+}
